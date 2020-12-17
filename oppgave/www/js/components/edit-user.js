f62950d87b28a2ca8f892c671e6564a2eca6c2b8
Marius Fredriksen <mkfredri@stud.ntnu.no>
2020-12-17 12:07:41 +0100

    somewhat done

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -3,28 +3,65 @@ import { LitElement, html, css } from "../../node_modules/lit-element/lit-elemen
 class EditUser extends LitElement {
   static get properties() {
     return {
-      user: { type: Object }
+      user: { type: Object },
     };
   }
 
   
   constructor(){
     super();
-  
-    fetch('api/fetchUser.php'+ , {
-      method: 'GET',
-      credentials: "include"
-  }).then(res => res.json()
-  ).then(data => {
-      console.log(data);
-      this.users = JSON.parse(JSON.stringify(data));
-  });
+
   }
 
 
 
 
 
+  render(){
+    return html`
+<form id="accountinfo" method="post">          
+<label>
+   <div>User name</div>
+   <input type="text" name="uname" value="${this.user.uname}" >
+ </label>
+
+<label>
+ <div>First name</div>
+ <input type="text" name="firstName" value="${this.user.firstName}" >
+</label>
+
+<label>
+  <div>Last name</div>
+  <input type="text" name="lastName" value="${this.user.lastName}" >
+</label>
+
+<label>
+  <div>Old password</div>
+  <input type="password" name="oldpwd"  value="${this.user.oldpwd}" >
+</label>
+
+<label>
+  <div>New password</div>
+  <input type="password" name="pwd">
+</label>
+
+<button type="button" value="Submit" @click="${this.updateInfo}">Update information</button>  
+</form>
+    
+    `
+  }
+
+
+
+
+  updateInfo(e){
+    fetch('api/updateUser.php', {
+      method : 'POST',
+    }).then(res => res.json())
+    .then(data=>{
+      console.log(data);
+    })
+  }
     
 
 }
